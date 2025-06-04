8bb9fdd701f641d8f40b0fc580d61c9725204e45
Alens Aleksandrs Čerņa <info@alens.lv>
2025-06-05 01:11:07 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1>src/App.vue has been loaded successfully</h1>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h2>You are currently accessing src/views/HomeView.vue</h2>
+</template>
